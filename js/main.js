c79837e432acf39d862eaed867aee244985be816
Pablo Mondino De La Rossa <p_mondino@hotmail.com>
2023-09-07 23:49:59 -0300

    guardando cambios

--- js/main.js
+++ js/main.js
@@ -1,41 +1,25 @@
 let pin = "1234";
 let ingresar = false;
-/*
-function ingreso() {
-  console.log(ingresar);
+const btnSearch = document.querySelector("#btnSearch");
+const inputIngreso = document.querySelector("#ingreso");
+const contenedor = document.querySelector("#contenedor");
+const infoLoteDiv = document.querySelector("#infoLote"); // Agrega un div para mostrar la información del lote
 
-  // alert("Bienvenido a la calculadora de cuotas para acceder a tu lote");
-  for (let i = 0; i <= 2; i++) {
-    let intento = prompt("ingrese numero de pin para acceder a al sistema");
-    if (intento == pin) {
-      alert("ingresaste correctamente, Bienvenido a sistema para calcular cuota");
-      ingresar = "true";
 
-      break;
-    } else {
-      alert("Lo siento, este pin no es correcto. Usaste tu intento N°" + (i + 1))
-    }
-  }
-}
 
-ingreso();
-console.log(ingresar);
-*/
+
+
+
+
 ingresar = true;
 
 
-//--------------------------
-
-const btnSearch = document.querySelector("#btnSearch"),
-  inputIngreso = document.querySelector("#ingreso");
-const contenedor = document.querySelector("#contenedor");
-//--------------------------------
 
 const tipoDeLotesDisponibles = [
-  { tipologia: "a", tamano: 250, frente: 10, fondo: 25, ubicacion: "Esquina de manzana", img: "250metros.png"},
-  { tipologia: "b", tamano: 360, frente: 12, fondo: 30, ubicacion: "Dentro de manzana",  img: "350metros.png"},
-  { tipologia: "c", tamano: 450, frente: 15, fondo: 30, ubicacion: "Dentro de manzana",  img: "450metros.png"},
-  { tipologia: "d", tamano: 560, frente: 16, fondo: 35, ubicacion: "Dentro de manzana",  img: "550metros.png"}];
+  { tipologia: "a", tamano: 250, frente: 10, fondo: 25, ubicacion: "Esquina de manzana", img: "250metros.png" },
+  { tipologia: "b", tamano: 360, frente: 12, fondo: 30, ubicacion: "Dentro de manzana", img: "350metros.png" },
+  { tipologia: "c", tamano: 450, frente: 15, fondo: 30, ubicacion: "Dentro de manzana", img: "450metros.png" },
+  { tipologia: "d", tamano: 560, frente: 16, fondo: 35, ubicacion: "Dentro de manzana", img: "550metros.png" }];
 
 
 
@@ -44,13 +28,27 @@ const tipoDeLotesDisponibles = [
 
 
 
-//Funciones de búsqueda
+
 function buscarServicio(arr, filtro) {
   const encontrado = arr.find((el) => {
     return el.tipologia.includes(filtro);//cambie nombre por tipologia
   });
   return encontrado;
 }
+
+
+
+
+function mostrarLotes(arr) {
+  const muestra = arr.map(element => {
+    return element;
+  });
+  return muestra;
+}
+
+
+
+
 function filtrarServicio(arr, filtro) {
   const filtrado = arr.filter((el) => {
     return el.tipologia.includes(filtro);// cambie nombre por tipologia
@@ -62,18 +60,18 @@ function crearHtml(arr) {
   let html;
   for (const el of arr) {
 
-    //cambie nombre por tipologia cambie img por 
-//<img src=" ./img/${el.img}" alt="${el.tipologia}">
+   
 
     html = `<div class="card">
     <img src=" ./img/${el.img}" alt="${el.tipologia}"> 
                 <hr>
                 <h3>${el.ubicacion}</h3>
-                <p>Precio: $${el.tamano} </p>
+                <p>Precio: $${el.tamano*1000} </p>
                   <div class="card-action">
                     <button class="btn btn-delete" id="${el.frente}">Quitar</button>
                   </div>
               </div>`;
+
     //se la agrego al contenedor
     contenedor.innerHTML = contenedor.innerHTML + html;
   }
@@ -82,6 +80,12 @@ function crearHtml(arr) {
 btnSearch.addEventListener("click", (e) => {
   const filtrados = filtrarServicio(tipoDeLotesDisponibles, inputIngreso.value);
   crearHtml(filtrados);
+});
+
+
+ingreso1.addEventListener("click", (e) => {
+  const mostrados = mostrarLotes(tipoDeLotesDisponibles);
+  crearHtml(mostrados);
 });
 
 
@@ -97,144 +101,37 @@ btnSearch.addEventListener("click", (e) => {
 
 
 
-/*
-if (ingresar) {
+btnSearch.addEventListener("click", (e) => {
+  const tipologiaBuscada = inputIngreso.value.toLowerCase(); // Convierte la entrada a minúsculas
+  const loteSeleccionado = buscarServicio(tipoDeLotesDisponibles, tipologiaBuscada);
 
-  //let valorTotalTerreno = 1000;
-  let valorTotalTerreno = 0;
-  let saldo = 0;
-  let cantidadDeCuotas = 1;
-  let entregaEnEfectivo = 0;
-  let tipologiaElegida = "";
-  let eleccion = [];
-
-  const tipoDeLotesDisponibles = [
-    { tipologia: "a", tamano: 250, frente: 10, fondo: 25, ubicacion: "Esquina de manzana" },
-    { tipologia: "b", tamano: 360, frente: 12, fondo: 30, ubicacion: "Dentro de manzana" },
-    { tipologia: "c", tamano: 450, frente: 15, fondo: 30, ubicacion: "Dentro de manzana" },
-    { tipologia: "d", tamano: 560, frente: 16, fondo: 35, ubicacion: "Dentro de manzana" }];
-
-  let elegirOpcion = prompt("Elegí tu opcion\n 1-Mostrar todos los tipos de lotes disponibles\n 2-Elegir tipología(a-b-c-d)\n 3-Valor total del terreno elegido\n 4-De cuanto es la entrega en efectivo que desea realizar? \n 5-¿Cual es el saldo para financiar?\n 6-¿En cuantas cuotas desea financiar el saldo?\n 7-Cómo quedaría el plan de financiación elegido\n Presione x para salir ");
-  while (elegirOpcion != "x") {
-    switch (elegirOpcion) {
-
-      case "1":
-        alert(" Elegiste 1- Mostrar todos los lotes disponibles por tipología ");
-        //alert( "lote 1- lote 2 - lote 3");
-        tipoDeLotesDisponibles.forEach(element => {
-          alert("Tipología: " + element.tipologia + "\nTamaño de lote: " + element.tamano + "\n Ubicación: " + element.ubicacion + "\n Medidas\n frente: " + element.frente + "\n fondo: " + element.fondo);
-
-          //console.log(element);
-
-        });
-        //alert(tipoDeLotesDisponibles);
-        break;
-
-      case "2":
-        function filtrarPorTipologia(arr, filtro) {
-          return arr.filter(elem => elem.tipologia == filtro);
-
-        }
-        tipologiaElegida = prompt("Elejiste opción 2.\n Elija una tipología (a,b,c,d)");
-
-        console.log(tipologiaElegida);
-        eleccion = filtrarPorTipologia(tipoDeLotesDisponibles, tipologiaElegida);
-        console.log();
-
-
-
-
-        alert(" Ud eligió la tipología: " + eleccion[0].tipologia);
-        console.log(eleccion[0]);
-
-        valorTotalTerreno = eleccion[0].tamano * 1000;
-        //nota el valor del metro cuadrado es de 1000 pesos
-        console.log(valorTotalTerreno)
-
-
-        break;
-
-
-
-
-
-
-      case "3":
-
-        if (valorTotalTerreno != 0) {
-          alert("Elegiste la opción 3\n  El valor total del terreno es de " + valorTotalTerreno + " pesos");
-
-
-          console.log(eleccion[0].tamano)
-        } else {
-          alert("Debe elegir una tipología")
-
-
-        }
-
-        break;
-
-
-      case "4":
-        entregaEnEfectivo = parseFloat(prompt("Elegiste la opcion 4\n ¿Cuanto es la entrega en efectivo que desea realizar?"));
-        4
-        //if (entregaEnEfectivo >= 200) {
-        if (entregaEnEfectivo >= (valorTotalTerreno * 0.20)) {
-          saldo = valorTotalTerreno - entregaEnEfectivo;
-          alert("Usted desea entregar " + entregaEnEfectivo + " pesos\n Tu entrega está correcta. Supera o es igual al monto minimo que es de por lo menos al 20% del valor del lote. ");
-
-
-
-        } else {
-          alert(" Tu entrega dede ser por lo menos igual o superior al 20% del valor del lote elegido. En este caso tu lote tiene un valor de " + valorTotalTerreno + " pesos, por lo que la entrega mínima debería ser de por lo menos " + (valorTotalTerreno * 0.20) + " pesos")
-        }
-
-        break;
-
-      case "5":
-        alert("Elegiste la opción 5\n  El saldo que desea financiar es de " + saldo + " pesos");
-
-        break;
-
-      case "6":
-        cantidadDeCuotas = parseFloat(prompt("Elija una opcion : \n puede elegir de 2 a 24 cuotas"));
-        if (cantidadDeCuotas <= 24 && cantidadDeCuotas >= 2) {
-          alert("elegiste " + cantidadDeCuotas + " cuotas")
-        } else {
-          alert("las cuotas deben ser entre  2 y 24 cuotas");
-        }
-
-
-        break;
-
-
-
-
-      case "7":
-        alert("Elegiste la opción 7 \n Valor total del lote es de " + valorTotalTerreno + " pesos. La entrega inicial es de " + entregaEnEfectivo + " y el saldo de " + saldo + " pesos a pagar en " + cantidadDeCuotas + " de cuotas de " + (saldo / cantidadDeCuotas) + " pesos");
-        break;
-
-
-
-
-
-
-
-      default:
-        alert("Esta opcion no válida");
-
-        break;
-
-
-    }
-    elegirOpcion = prompt("Elegí tu opcion\n 1-Mostrar todos los tipos de lotes disponibles\n 2-Elegir tipología(a-b-c-d)\n 3-Valor total del terreno elegido\n 4-De cuanto es la entrega en efectivo que desea realizar? \n 5-¿Cual es el saldo para financiar?\n 6-¿En cuantas cuotas desea financiar el saldo?\n 7-Cómo quedaría el plan de financiación elegido \n Presione x para salir ");
+  if (loteSeleccionado) {
+    mostrarInformacionLote(loteSeleccionado);
+  } else {
+    infoLoteDiv.innerHTML = "Lote no encontrado";
   }
-  alert("gracias por operar con nosotros");
+});
 
+// Función para mostrar la información de un lote en el div infoLote
+function mostrarInformacionLote(lote) {
+  const html = `
+    <div class="info-lote">
+      <img src="./img/${lote.img}" alt="${lote.tipologia}">
+      <h2>Tipología: ${lote.tipologia}</h2>
+      <p>Tamaño de lote: ${lote.tamano} metros cuadrados</p>
+      <p>Ubicación: ${lote.ubicacion}</p>
+      <p>Medidas:</p>
+      <ul>
+        <li>Frente: ${lote.frente} metros</li>
+        <li>Fondo: ${lote.fondo} metros</li>
+      </ul>
+      <p>Precio Contado: ${(lote.tamano)*1000}</p>
+
+    </div>
+  `;
+
+  infoLoteDiv.innerHTML = html;
 }
-else {
-  alert("No podrás seguir operando. LLama a nuestra oficina para solicitar un nuevo pin.")
 
-}
 
-*/
+
